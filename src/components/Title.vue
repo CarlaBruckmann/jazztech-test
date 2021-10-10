66d40de18b855356114bb06422ddeed87bcf7e3a
Carla Bruckmann <carlaabruckmann@gmail.com>
2021-10-10 00:59:17 -0300

    Added line and edited text

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,20 +1,22 @@
 <template>
-    <div>
-        <h1>Relatório de Prospecção</h1>
-    </div>
+  <div>
+    <h1>Relatório de Prospecção</h1>
+    <hr>
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'Title'
-}
+  name: "Title",
+};
 </script>
 
 <style scoped>
 h1 {
-font-family: 'Exo', sans-serif;
-font-size: 24px;
-text-transform: uppercase;
+  padding-bottom: 10px;
+  font-family: "Exo", sans-serif;
+  font-size: 24px;
+  text-transform: uppercase;
+  color: var(--white);
 }
 </style>
